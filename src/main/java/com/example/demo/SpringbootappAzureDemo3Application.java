2f9ffbc7be88ddc6e24483f905b0e9194b99ff57
Jha <sachin.jha@dxc.com>
2022-06-10 10:48:04 +0530

    first commit

---
+++ src/main/java/com/example/demo/SpringbootappAzureDemo3Application.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootappAzureDemo3Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootappAzureDemo3Application.class, args);
+	}
+
+}
